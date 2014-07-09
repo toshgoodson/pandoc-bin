3570b187d619222b4398e99313e6939273da1c8f
Tosh Goodson <tosh.goodson@gmail.com>
2014-07-09 14:50:35 -0700

    Init commit; bits of working code

---
+++ readme.md
@@ -0,0 +1,42 @@
+# pandoc-bin
+
+[Pandoc](https://github.com/jgm/pandoc) 1.12.4.2 Node.js wrapper that makes it seamlessly available as a local dependency on OS X and Windows (Linux coming soon).
+
+> OptiPNG is a PNG optimizer that recompresses image files to a smaller size, without losing any information.
+
+
+## Install
+
+```bash
+$ npm install --save pandoc-bin
+```
+
+
+## Usage
+
+```js
+var execFile = require('child_process').execFile;
+var optipng = require('pandoc-bin').path;
+
+execFile(optipng, ['-v'], function (err, stdout, stderr) {
+    console.log('Pandoc version:', stdout.match(/\d\.\d\.\d(\.\d)?/)[0]);
+});
+```
+
+
+## CLI
+
+```bash
+$ npm install --global pandoc-bin
+```
+
+```bash
+$ pandoc --help
+```
+
+
+## License
+
+Everything excluding the binaries licensed under the [MIT license](http://opensource.org/licenses/MIT).
+
+Pandoc is licensed under the [GPL v2](http://www.gnu.org/licenses/gpl-2.0.html) and copyright John MacFarlane and the Contributing Authors.
---
+++ test/test.js
@@ -0,0 +1,59 @@
+/*global afterEach, beforeEach, describe, it */
+'use strict';
+
+var assert = require('assert');
+var binCheck = require('bin-check');
+var BinBuild = require('bin-build');
+var fs = require('fs');
+var path = require('path');
+var spawn = require('child_process').spawn;
+var rm = require('rimraf');
+
+describe('optipng()', function () {
+	afterEach(function (cb) {
+		rm(path.join(__dirname, 'tmp'), cb);
+	});
+
+	beforeEach(function (cb) {
+		fs.mkdir(path.join(__dirname, 'tmp'), cb);
+	});
+
+	it('should rebuild the optipng binaries', function (cb) {
+		var tmp = path.join(__dirname, 'tmp');
+		var builder = new BinBuild()
+			.src('http://downloads.sourceforge.net/project/optipng/OptiPNG/optipng-0.7.5/optipng-0.7.5.tar.gz')
+			.cfg('./configure --with-system-zlib --prefix="' + tmp + '" --bindir="' + tmp + '"')
+			.make('make install');
+
+		builder.build(function (err) {
+			assert(!err);
+			assert(fs.existsSync(path.join(tmp, 'optipng')));
+			cb();
+		});
+	});
+
+	it('should return path to binary and verify that it is working', function (cb) {
+		var binPath = require('../').path;
+
+		binCheck(binPath, '--version', function (err, works) {
+			cb(assert.equal(works, true));
+		});
+	});
+
+	it('should minify a PNG', function (cb) {
+		var binPath = require('../').path;
+		var args = [
+			'-strip', 'all',
+			'-clobber',
+			'-out', path.join(__dirname, 'tmp/test.png'),
+			path.join(__dirname, 'fixtures', 'test.png')
+		];
+
+		spawn(binPath, args).on('close', function () {
+			var src = fs.statSync(path.join(__dirname, 'fixtures/test.png')).size;
+			var dest = fs.statSync(path.join(__dirname, 'tmp/test.png')).size;
+
+			cb(assert(dest < src));
+		});
+	});
+});
