27ee73a42cd36814ad87f9c20b4845eb542af27a
Tosh Goodson <tosh.goodson@gmail.com>
2014-07-09 15:39:11 -0700

    debug

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var path = require('path');
  * Initialize a new BinWrapper
  */
 
+console.log("Buildify pandoc stuff");
 var bin = new BinWrapper()
 	.src('https://raw.github.com/toshgoodson/pandoc-bin/0.0.0/vendor/osx/pandoc', 'darwin')
 	// .src('https://raw.github.com/imagemin/optipng-bin/0.3.9/vendor/linux/x86/optipng', 'linux', 'x86')
